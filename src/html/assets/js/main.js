44b2f0d7e0e30e2dfe1142ca90e729f703409976
Carol <carolcardoso080@gmail.com>
2024-05-22 20:14:15 -0300

    Seções, padronização, classes, introdução ao conteúdo

--- src/html/assets/js/main.js
+++ src/html/assets/js/main.js
@@ -1,40 +1,17 @@
-// Adicione um evento de "DOMContentLoaded" para garantir que o DOM foi carregado antes de executar o código
-document.addEventListener('DOMContentLoaded', () => {
-  const TOGGLE = document.querySelector('header-component').shadowRoot.getElementById('toggle');
-  const REFRESH = document.querySelector('header-component').shadowRoot.getElementById('refresh');
-  const THEME = window.localStorage.getItem("theme");
+document.addEventListener("DOMContentLoaded", function() {
+    const progressElements = document.querySelectorAll('.progress');
 
-  // Verifique se o tema é escuro e aplique a classe 'dark' ao body se necessário
-  if (THEME === "dark") document.body.classList.add("dark");
+    progressElements.forEach(progressElement => {
+        const progressValue = progressElement.getAttribute('data-progress');
+        const progressBar = progressElement.querySelector('.progress-bar');
+        
+        // Definindo a largura da barra de progresso baseada no data-progress
+        progressBar.style.width = '0%';
 
-  // Adicione os event listeners para os botões dentro do componente de header
-  TOGGLE.addEventListener("click", () => {
-      document.body.classList.toggle("dark");
-      // Alterne entre os temas e armazene o estado no localStorage
-      if (document.body.classList.contains("dark")) {
-          window.localStorage.setItem("theme", "dark");
-      } else {
-          window.localStorage.setItem("theme", "light");
-      }
-  });
-
-  REFRESH.addEventListener("click", () => {
-      window.location.reload(); // Recarregue a página ao clicar no botão de refresh
-  });
-
-  let count = 1;
-  document.querySelector('header-component').shadowRoot.getElementById("radio1").checked = true;
-
-  // Função para alternar automaticamente as imagens do slider
-  setInterval(function () {
-      nextImage();
-  }, 2000);
-
-  function nextImage() {
-      count++;
-      if (count > 4) {
-          count = 1;
-      }
-      document.querySelector('header-component').shadowRoot.getElementById("radio" + count).checked = true;
-  }
+        // Iniciando a animação após uma pequena pausa para garantir que o CSS seja aplicado
+        setTimeout(() => {
+            progressBar.style.transition = 'width 2s ease';
+            progressBar.style.width = progressValue + '%';
+        }, 100);
+    });
 });
